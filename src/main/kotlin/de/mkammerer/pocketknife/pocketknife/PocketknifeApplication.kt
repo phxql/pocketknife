b75311cd74f02b7f665b558e61d9b27e3266713f
Moritz Kammerer <kammerer.moritz@gmail.com>
2018-03-23 18:22:18 +0100

    Import project from Spring Initializr
    Update Kotlin to 1.2.31 and Gradle Wrapper to 4.6

---
+++ src/main/kotlin/de/mkammerer/pocketknife/pocketknife/PocketknifeApplication.kt
@@ -0,0 +1,11 @@
+package de.mkammerer.pocketknife.pocketknife
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PocketknifeApplication
+
+fun main(args: Array<String>) {
+    runApplication<PocketknifeApplication>(*args)
+}
---
+++ src/test/kotlin/de/mkammerer/pocketknife/pocketknife/PocketknifeApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.mkammerer.pocketknife.pocketknife
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PocketknifeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
