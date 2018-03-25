548b7f0f30f33bcef5f055c5a092f34b80708170
Moritz Kammerer <kammerer.moritz@gmail.com>
2018-03-25 16:02:03 +0200

    How iat, exp and nbf as ISO8601 when decoding JWT

--- src/main/kotlin/de/mkammerer/pocketknife/pocketknife/json/JwtDecode.kt
+++ src/main/kotlin/de/mkammerer/pocketknife/pocketknife/json/JwtDecode.kt
@@ -1,6 +1,7 @@
 package de.mkammerer.pocketknife.pocketknife.json
 
 import com.fasterxml.jackson.core.JsonProcessingException
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import de.mkammerer.pocketknife.pocketknife.BaseController
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.servlet.ModelAndView
+import java.time.Instant
 import java.util.*
 
 @Controller
@@ -34,9 +36,21 @@ class JwtDecode : BaseController<JwtDecode.Model>("jwt/decode") {
         if (parts.size < 2) throw IllegalArgumentException("Expected at least 2 parts, found only ${parts.size}")
 
         val header = prettifyJson(Base64.getDecoder().decode(parts[0]))
-        val payload = prettifyJson(Base64.getDecoder().decode(parts[1]))
 
-        return Token(header, payload)
+        val decodedPayload = Base64.getDecoder().decode(parts[1])
+        val parsedPayload = ObjectMapper().readTree(decodedPayload)
+
+        val exp = if (parsedPayload.has("exp")) formatTimestamp(parsedPayload.get("exp")) else null
+        val nbf = if (parsedPayload.has("nbf")) formatTimestamp(parsedPayload.get("nbf")) else null
+        val iat = if (parsedPayload.has("iat")) formatTimestamp(parsedPayload.get("iat")) else null
+
+        val payload = prettifyJson(decodedPayload)
+        return Token(header, payload, exp, nbf, iat)
+    }
+
+    private fun formatTimestamp(node: JsonNode): String {
+        val timestamp = node.asLong()
+        return Instant.ofEpochSecond(timestamp).toString()
     }
 
     private fun prettifyJson(input: ByteArray): String {
@@ -48,16 +62,22 @@ class JwtDecode : BaseController<JwtDecode.Model>("jwt/decode") {
         }
     }
 
-    data class Token(val header: String, val payload: String)
+    data class Token(
+            val header: String,
+            val payload: String,
+            val exp: String?,
+            val nbf: String?,
+            val iat: String?
+    )
 
     data class DecodeForm(val jwt: String)
-    data class Model(val jwt: String, val header: String, val payload: String, val error: String?) {
+    data class Model(val jwt: String, val header: String, val payload: String, val exp: String?, val nbf: String?, val iat: String?, val error: String?) {
         companion object {
-            val EMPTY = Model("", "", "", null)
+            val EMPTY = Model("", "", "", null, null, null, null)
 
-            fun fromToken(jwt: String, token: Token) = Model(jwt, token.header, token.payload, null)
+            fun fromToken(jwt: String, token: Token) = Model(jwt, token.header, token.payload, token.exp, token.nbf, token.iat, null)
 
-            fun fromException(jwt: String, exception: Exception) = Model(jwt, "", "", exception.message)
+            fun fromException(jwt: String, exception: Exception) = Model(jwt, "", "", null, null, null, exception.message)
         }
     }
 }
